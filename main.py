798299c34e2a5f252615b8ddd9051c9ca6b6d920
Anna Koldova <AK240@statoilfuelretail.com>
2021-11-21 14:50:22 +0000

    looking for duplicates

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ sales = pd.read_csv(r"C:\Users\ak240\Desktop\Data Analytics Course\Salesstore.cs
 print(sales.shape)
 print(sales.head(10))
 
+#filtering
 print(sales['Ship_Mode'] == 'Express Air')
 filt = (sales['Ship_Mode'] == 'Express Air')
 print(sales[filt])
@@ -15,4 +16,12 @@ print(sales[filt])
 print(sales['Ship_Mode'] == 'Regular Air')
 filt2 = (sales['Ship_Mode'] == 'Regular Air')
 print(sales[filt2])
+
+print(sales.loc[sales['Customer_Segment'] == 'Corporate'])
+print(sales.loc[(sales['Customer_Segment'] == 'Corporate') & (sales['Ship_Mode'] == 'Express Air') & (sales['Order_Quantity'] > 10)])
+
+#duplicates
+print(sales.duplicated(subset=['Order_ID', 'Sales']).sum())
+
+
 
