b3b37247dcec248d0106ff58e387c7ff7bd46b2b
Anna Koldova <AK240@statoilfuelretail.com>
2021-11-21 19:33:29 +0000

    changed duplicates command to group by oder id

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ filt2 = (sales['Ship_Mode'] == 'Regular Air')
 print(sales[filt2])
 
 print(sales.loc[sales['Customer_Segment'] == 'Corporate'])
-print(sales.loc[(sales['Customer_Segment'] == 'Corporate') & (sales['Ship_Mode'] == 'Express Air') & (sales['Order_Quantity'] > 10)])
-
-#duplicates
-print(sales.duplicated(subset=['Order_ID', 'Sales']).sum())
-print(sales.duplicated(subset=['Order_ID']))
+print(sales.loc[(sales['Customer_Segment'] == 'Corporate') & (sales['Ship_Mode'] == 'Express Air') & (sales['Order_Quantity'] >= 0)])
 
 #group by
 print(sales.groupby(['Region']).count())
 
+#duplicates
 
+print(sales[sales['Order_ID'].duplicated(keep=False)])
+sales['Grouped'] = sales.groupby('Order_ID')['Product_Category'].transform(lambda x: ','.join(x))
+sales=sales[['Order_ID', 'Grouped']].drop_duplicates()
+
+print(sales)
