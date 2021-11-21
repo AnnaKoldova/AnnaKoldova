d9a8cf59169b4c189bce450f9a39b4adca86f94a
Anna Koldova <AK240@statoilfuelretail.com>
2021-11-21 14:07:03 +0000

    filtering command

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #upload CSV file
-
 sales = pd.read_csv(r"C:\Users\ak240\Desktop\Data Analytics Course\Salesstore.csv")
-print(sales)
+print(sales.shape)
+print(sales.head(10))
+
+print(sales['Ship_Mode'] == 'Express Air')
+filt = (sales['Ship_Mode'] == 'Express Air')
+print(sales[filt])
+
+print(sales['Ship_Mode'] == 'Regular Air')
+filt2 = (sales['Ship_Mode'] == 'Regular Air')
+print(sales[filt2])
 
