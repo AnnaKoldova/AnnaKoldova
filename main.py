ee77f434997bbd69a34195cd54236772484df28b
Anna Koldova <AK240@statoilfuelretail.com>
2021-11-21 12:55:45 +0000

    uploaded CSV file with pandas

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#upload CSV file
+
+sales = pd.read_csv(r"C:\Users\ak240\Desktop\Data Analytics Course\Salesstore.csv")
+print(sales)
+
