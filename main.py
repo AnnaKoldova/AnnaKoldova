86ca28d1621272fc3be0a0463a81c54e23bbba41
Anna Koldova <AK240@statoilfuelretail.com>
2021-11-21 23:28:21 +0000

    Project report added

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-#upload CSV file
+# upload CSV file
 sales = pd.read_csv(r"C:\Users\ak240\Desktop\Data Analytics Course\Salesstore.csv")
 print(sales.shape)
 print(sales.head(10))
 
-#filtering
+# filtering
 print(sales['Ship_Mode'] == 'Express Air')
 filt = (sales['Ship_Mode'] == 'Express Air')
 print(sales[filt])
@@ -22,7 +21,7 @@ print(sales.loc[sales['Customer_Segment'] == 'Corporate'])
 print(sales.loc[(sales['Customer_Segment'] == 'Corporate')
                 & (sales['Ship_Mode'] == 'Express Air')
                 & (sales['Order_Quantity'] >= 0)])
-#groupes most common
+# groups most common
 
 from itertools import combinations
 from collections import Counter
@@ -34,20 +33,20 @@ for row in sales['Grouped']:
 
 for key, value in count.most_common():
     print(key, value)
-#group by
+# group by
 print(sales.groupby(['Region']).count())
 
-#duplicates
+# duplicates
 
 print(sales[sales['Order_ID'].duplicated(keep=False)])
 sales['Grouped'] = sales.groupby('Order_ID')['Product_Category'].transform(lambda x: ','.join(x))
 sales = sales[['Order_ID', 'Grouped']].drop_duplicates()
 print(sales)
 
-plot_11=sns.regplot(data=sales,x='Sales',y='Profit')
+plot_11 = sns.regplot(data=sales, x='Sales', y='Profit')
 plt.show()
 
-#visualisation
+# visualisation
 
 plot_1 = sns.histplot(data=sales, x='Ship_Mode')
 plt.show()
@@ -76,11 +75,3 @@ plt.show()
 plot_8 = sns.barplot(data=sales, x='Region', y='Profit', hue='Product_Category')
 plt.xticks(rotation=45)
 plt.show()
-
-
-
-
-
-
-
-
